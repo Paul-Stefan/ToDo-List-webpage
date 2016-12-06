adb8982ae49122489ac20425915594362cb18e82
Paul-Stefan <paulstefandutoit@gmail.com>
2016-12-06 12:27:47 +0200

    added class lables nothing significant yet
    ToDo:
    pdynamically create class names
    *add class names to array for sorting, hiding and showing

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 function createTask(){
+
 	var listAreaId = document.getElementById("toDoListArea");
-	console.log(listAreaId);
 	//creates checkbox for net task
 	var newTaskCB = document.createElement("INPUT");
     newTaskCB.setAttribute("type", "checkbox");
+	newTaskCB.setAttribute("class", "hos");
     listAreaId.appendChild(newTaskCB);
 	//creates new lable for text box
-	var taskName = document.getElementById("myText").value;
-	//var taskName = textBoxId.getAttribute("value");
-	
-	var taskLable = document.createTextNode(taskName);
+	var taskName = document.getElementById("myText").value;	
+	 //var taskLable = document.createTextNode(taskName);
+	var taskLable = document.createElement("p");
+	taskLable.textContent = taskName;
+	taskLable.setAttribute("class", "hos");
 	listAreaId.appendChild(taskLable);
+	
 }
+
+
 
 function approve(){
 	$( document ).ready(createTask());
+}
+
+function showOrHide(){
+	$( ".hos" ).remove();
 }
