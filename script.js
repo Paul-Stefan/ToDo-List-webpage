bdd22cf6efc24fde50e098d559290764e43d3707
Paul-Stefan <paulstefandutoit@gmail.com>
2016-12-06 13:36:39 +0200

    Updated script.js
    Dynamically creates class names, hides checked tasks
    ToDo: show hidden tasks again

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-function createTask(){
+var counter = 0;
+var classNames = [];
 
+function createTask(){
+counter ++;
 	var listAreaId = document.getElementById("toDoListArea");
 	//creates checkbox for net task
 	var newTaskCB = document.createElement("INPUT");
     newTaskCB.setAttribute("type", "checkbox");
-	newTaskCB.setAttribute("class", "hos");
+	newTaskCB.setAttribute("class", counter);
     listAreaId.appendChild(newTaskCB);
 	//creates new lable for text box
 	var taskName = document.getElementById("myText").value;	
 	 //var taskLable = document.createTextNode(taskName);
 	var taskLable = document.createElement("p");
 	taskLable.textContent = taskName;
-	taskLable.setAttribute("class", "hos");
+	taskLable.setAttribute("class", counter);
 	listAreaId.appendChild(taskLable);
 	
+	classNames.push(counter);
 }
-
-
 
 function approve(){
 	$( document ).ready(createTask());
 }
 
 function showOrHide(){
-	$( ".hos" ).remove();
+	var arrayLength = classNames.length;
+	var checked;
+	
+	for(var i = 0; i < arrayLength; i++){
+		console.log(classNames[i]);
+		checked = ($('.' + classNames[i] + '').prop("checked"));
+		if(checked == true){
+			$('.' + classNames[i] + '').hide();
+		}
+	}
 }
